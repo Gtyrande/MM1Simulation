e2a46ac9cc729e03def52274119971fd0044ea6c
Gtyrande <GtYrande.YC@hotmail.com>
2018-04-19 13:48:12 +0800

    Initial commit

---
+++ MM1Simulation/MersenneTwisterTool.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShootProbabilityTool
+{
+    public class MersenneTwister : Random
+    {
+
+        private const Int32 N = 624;
+
+        private const Int32 M = 397;
+
+        private const UInt32 MatrixA = 0x9908b0df; /* constant vector a */
+
+        private const UInt32 UpperMask = 0x80000000; /* most significant w-r bits */
+
+        private const UInt32 LowerMask = 0x7fffffff; /* least significant r bits */
+
+        private const UInt32 TemperingMaskB = 0x9d2c5680;
+
+        private const UInt32 TemperingMaskC = 0xefc60000;
+
+
+
+        private readonly UInt32[] _mt = new UInt32[N]; /* the array for the state vector */
+
+        private Int16 _mti;
+
+        private static readonly UInt32[] _mag01 = { 0x0, MatrixA };
+
+        //初始化seed
+        public MersenneTwister(Int32 seed)
+        {
+
+            init((UInt32)seed);
+
+        }
+
+        //生成随机数
+        public override Double NextDouble()
+        {
+            Double FiftyThreeBitsOf1s = 9007199254740991.0;
+
+            Double Inverse53BitsOf1s = 1.0 / FiftyThreeBitsOf1s;
+
+            Double OnePlus53BitsOf1s = FiftyThreeBitsOf1s + 1;
+
+            Double InverseOnePlus53BitsOf1s = 1.0 / OnePlus53BitsOf1s;
+
+            UInt64 a = (UInt64)GenerateUInt32() >> 5;
+
+            UInt64 b = (UInt64)GenerateUInt32() >> 6;
+
+            return ((a * 67108864.0 + b) + 0) * InverseOnePlus53BitsOf1s;
+        }
+
+
+        protected UInt32 GenerateUInt32()
+        {
+
+            UInt32 y;
+
+            if (_mti >= N)
+            {
+
+                Int16 kk = 0;
+
+                for (; kk < N - M; ++kk)
+                {
+
+                    y = (_mt[kk] & UpperMask) | (_mt[kk + 1] & LowerMask);
+
+                    _mt[kk] = _mt[kk + M] ^ (y >> 1) ^ _mag01[y & 0x1];
+
+                }
+
+                for (; kk < N - 1; ++kk)
+                {
+
+                    y = (_mt[kk] & UpperMask) | (_mt[kk + 1] & LowerMask);
+
+                    _mt[kk] = _mt[kk + (M - N)] ^ (y >> 1) ^ _mag01[y & 0x1];
+
+                }
+
+                y = (_mt[N - 1] & UpperMask) | (_mt[0] & LowerMask);
+
+                _mt[N - 1] = _mt[M - 1] ^ (y >> 1) ^ _mag01[y & 0x1];
+
+                _mti = 0;
+
+            }
+
+            y = _mt[_mti++];
+
+            y ^= y >> 11;
+
+            y ^= y << 7 & TemperingMaskB;
+
+            y ^= y << 15 & TemperingMaskC;
+
+            y ^= y >> 18;
+
+            return y;
+
+        }
+
+        private void init(UInt32 seed)
+        {
+
+            _mt[0] = seed & 0xffffffffU;
+
+            for (_mti = 1; _mti < N; _mti++)
+            {
+
+                _mt[_mti] = (uint)(1812433253U * (_mt[_mti - 1] ^ (_mt[_mti - 1] >> 30)) + _mti);
+
+                _mt[_mti] &= 0xffffffffU;
+
+            }
+
+        }
+
+    }
+
+}
+
---
+++ MM1Simulation/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM1Simulation
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        const int QLimit = 100;
+        const int BUSY = 1;
+        const int IDLE = 0;
+
+        static int nextEventType;
+        static int numCustsDelayed;
+        static int numDelaysRequired;
+        static int numEvents = 2;
+        static int numInQ;
+        static int serverStatus;
+
+        static double areaNumInQ;
+        static double areaServerStatus;
+        static double meanInterarrival;
+        static double meanService;
+        static double simTime;
+        static double timeLastEvent;
+        static double totalOfDelays;
+
+        static double[] timeArrival = new double[QLimit + 1];
+        static double[] timeNextEvent = new double[3];
+     
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //
+            Console.WriteLine("Input mean interarrival: ");
+            meanInterarrival = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input mean service time: ");
+            meanService = double.Parse(Console.ReadLine());
+            Console.WriteLine("Imput number of customers: ");
+            numDelaysRequired = int.Parse(Console.ReadLine());
+
+            //
+            Initialize();
+
+            //
+            while (numCustsDelayed < numDelaysRequired)
+            {
+                Timing();
+                UpdateTimeAvgStats();
+
+                //
+                switch (nextEventType)
+                {
+                    case 1:
+                        Arrive();
+                        break;
+                    case 2:
+                        Depart();
+                        break;
+                }
+            }
+
+            //
+            Console.Write("Average delay in queue: ");
+            Console.WriteLine(totalOfDelays / numCustsDelayed);
+            Console.Write("Average number in queue: ");
+            Console.WriteLine(areaNumInQ / simTime);
+            Console.Write("Server utilization: ");
+            Console.WriteLine(areaServerStatus / simTime);
+            Console.Write("Time simulation ended: ");
+            Console.WriteLine(simTime);
+            Console.WriteLine(numCustsDelayed);
+        }
+
+        static void Initialize()
+        {
+            //
+            simTime = 0;
+
+            //
+            serverStatus = IDLE;
+            numInQ = 0;
+            timeLastEvent = 0.0;
+
+            //
+            numCustsDelayed = 0;
+            totalOfDelays = 0;
+            areaNumInQ = 0;
+            areaServerStatus = 0;
+
+            //
+            timeNextEvent[1] = simTime + expon(meanInterarrival);
+            timeNextEvent[2] = 1.0e+30;
+        }
+
+        static void Timing()
+        {
+            double minTimeNextEvent = 1.0e+29;
+            nextEventType = 0;
+
+            //
+            for (int i = 1; i < numEvents + 1; i++)
+            {
+                if (timeNextEvent[i] < minTimeNextEvent)
+                {
+                    minTimeNextEvent = timeNextEvent[i];
+                    nextEventType = i;
+                }
+            }
+
+            //
+            simTime = minTimeNextEvent;
+        }
+
+        static void Arrive()
+        {
+            double delay;
+
+            //
+            timeNextEvent[1] = simTime + expon(meanInterarrival);
+            //
+            if (serverStatus == BUSY)
+            {
+                ++numInQ;
+                if (numInQ > QLimit)
+                {
+                    Console.WriteLine("Shit!");
+                }
+                timeArrival[numInQ] = simTime;
+            }
+            else
+            {
+                delay = 0;
+                totalOfDelays += delay;
+
+                ++numCustsDelayed;
+                serverStatus = BUSY;
+
+                timeNextEvent[2] = simTime + expon(meanService);
+            }
+        }
+
+        static void Depart()
+        {
+            double delay;
+
+            if (numInQ == 0)
+            {
+                //
+                serverStatus = IDLE;
+                timeNextEvent[2] = 1.0e+30;
+            }
+            else
+            {
+                //
+                --numInQ;
+
+                //
+                delay = simTime - timeArrival[1];
+                totalOfDelays += delay;
+
+                //
+                ++numCustsDelayed;
+                timeNextEvent[2] = simTime + expon(meanService);
+
+                for (int i = 1; i < numInQ + 1; i++)
+                {
+                    timeArrival[i] = timeArrival[i + 1];
+                }
+            }
+        }
+
+        static void UpdateTimeAvgStats()
+        {
+            double timeSinceLastEvent;
+
+            //
+            timeSinceLastEvent = simTime - timeLastEvent;
+            timeLastEvent = simTime;
+
+            //
+            areaNumInQ += numInQ * timeSinceLastEvent;
+            areaServerStatus += serverStatus * timeSinceLastEvent;
+        }
+
+        static double expon(double mean)
+        {
+            Lgcrand rand = new Lgcrand();
+            return -mean * Math.Log(rand.Lcgrand(3));
+        }
+    }
+}
